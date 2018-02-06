dbcc02c89d7eeb0b960f3cc7c1ffa4f0f602a33a
Salvatore D'Angelo <sdangelo@it.ibm.com>
2018-02-06 16:42:36 +0100

    Improved code

--- tiny_linux.sh
+++ tiny_linux.sh
@@ -1,10 +1,123 @@
-if [ -f /etc/redhat-release ]
-then
-    grep "Red Hat Enterprise Linux Workstation" /etc/redhat-release > /dev/null
-    if [ $? -eq 0 ]
+LINUX_KERNEL="linux-4.10.6.tar.xz"
+BUSYBOX="busybox-1.26.2.tar.bz2"
+LINUX_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/$LINUX_KERNEL"
+BUSYBOX_URL="https://busybox.net/downloads/$BUSYBOX"
+
+###################################################################
+# usage
+#
+# Input Parameters:
+#     none
+#
+# Description:
+#     This function prints the usage.
+#
+# Return:
+#     None
+###################################################################
+usage()
+{
+    echo "Usage: ./tiny_linux.sh"
+}
+
+###################################################################
+# operating_system_not_supported
+#
+# Input Parameters:
+#     none
+#
+# Description:
+#     This function prints the "Operating system not supported" error
+#     message.
+#
+# Return:
+#     None
+###################################################################
+operating_system_not_supported()
+{
+    echo "ERROR: Operating system not supported."
+    echo "       This script has been tested only on RHE Worksation."
+}
+
+###################################################################
+# parse_params
+#
+# Input Parameters:
+#     none
+#
+# Description:
+#     This function parse the input parameters.
+#
+# Return:
+#     None
+###################################################################
+parse_params()
+{
+    echo $*
+    if [ $# -gt 0 ]
     then
-    . ./rhew/tiny_linux.sh
+        usage
+        exit 1
     fi
-else
-    echo "Operating system not supported!"
-fi
+}
+
+###################################################################
+# check_prerequisites
+#
+# Input Parameters:
+#     none
+#
+# Description:
+#     This function checks prerequisites to build the tiny linux image.
+#
+# Return:
+#     None
+###################################################################
+check_prerequisites()
+{
+    if [ -f /etc/redhat-release ]
+    then
+        grep "Red Hat Enterprise Linux Workstation" /etc/redhat-release > /dev/null
+        if [ $? -ne 0 ]
+        then
+            operating_system_not_supported
+            exit 2
+        fi
+    else
+        operating_system_not_supported
+        exit 3
+    fi
+}
+
+###################################################################
+# Main block
+###################################################################
+parse_params "$@"
+check_prerequisites
+
+TOP=$HOME/tiny_linux
+mkdir -pv $TOP
+cd $TOP
+curl $LINUX_URL | tar xJf -
+curl $BUSYBOX_URL | tar xjf -
+cd $TOP/busybox-1.26.2
+mkdir -pv $TOP/obj/busybox-x86
+make O=$TOP/obj/busybox-x86 defconfig
+cp $TOP/obj/busybox-x86/.config $TOP/obj/busybox-x86/.config.old
+sed -i "s/# CONFIG_STATIC is not set/CONFIG_STATIC=y/g" $TOP/obj/busybox-x86/.config
+cd $TOP/obj/busybox-x86
+make -j2
+make install
+mkdir -pv $TOP/initramfs/x86-busybox
+cd $TOP/initramfs/x86-busybox
+mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+cp -av $TOP/obj/busybox-x86/_install/* .
+cp $TOP/init .
+chmod +x init
+find . -print0 \
+	| cpio --null -ov --format=newc \
+	| gzip -9 > $TOP/obj/initramfs-busybox-x86.cpio.gz
+cd $TOP/linux-4.10.6
+make O=$TOP/obj/linux-x86-basic x86_64_defconfig
+make O=$TOP/obj/linux-x86-basic kvmconfig
+make O=$TOP/obj/linux-x86-basic -j2
